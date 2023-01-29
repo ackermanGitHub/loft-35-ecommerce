611129c17e40fa7ac6b74e2baaf1c9768147df98
ackerman <julio.sergio2709@gmail.com>
2023-01-29 11:42:57 -0500

    I made a little changes

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -15,17 +15,20 @@ const ProductItem: React.FC<IProps> = ({ product }) => {
           src={product.image}
           alt={product.name}
           className="rounded shadow"
-          width={200}
-          height={200}
+          width={160}
+          height={160}
         />
       </Link>
-      <div className="flex flex-col items-center justify-center p-5">
+      <div className="flex flex-col items-center justify-center m-auto">
         <Link href={`/product/${product.slug}`}>
           <h2 className="text-lg">{product.name}</h2>
         </Link>
-        <p className="mb-2">{product.brand}</p>
+        <p>{product.brand}</p>
         <p>${product.price}</p>
-        <button className="primary-button" type="button">
+        <button
+          className="primary-button h-9 w-20 text-center text-xs"
+          type="button"
+        >
           Añadir al Carrito
         </button>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import data from '@/utils/data';
 export default function Home() {
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="flex flex-wrap justify-around max-w-2xl m-auto">
         {data.products.map((product): React.ReactNode => {
           return <ProductItem product={product} key={product.slug} />;
         })}
